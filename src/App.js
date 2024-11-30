3469449c372e650240fd5ee650dca853c1d3772a
deokbin <deokbin@naver.com>
2024-11-30 16:41:42 +0900

    feat(App): 클래스 및 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Input from "./Input.js";
+import InputValidator from "./InputValidator.js";
+import Car from "./Car.js";
+import Winner from "./Winner.js";
+import Output from "./Output.js";
+import splitCarName from "./utils/splitCarName.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await Input.getCarName();
+    const splittedCarNames = splitCarName(carNames);
+    InputValidator.validateCarName(splittedCarNames);
+
+    const tryCount = await Input.getTryCount();
+    InputValidator.validateTryCount(tryCount);
+
+    const car = new Car(splittedCarNames);
+    const raceResult = car.startRace(tryCount);
+    const winnerList = Winner.determineWinner(raceResult);
+    Output.printWinner(winnerList);
+  }
 }
 
 export default App;
